9968ceb4f5546ab1d54c17c672f3c85cfb78a848
Saul <saul_harry_@hotmail.com>
2021-12-21 20:51:09 -0600

    Se agregan cambios para utilizar un interceptor para agregar el token a la peticion en automatico en cada request, tambien se agrega configuracion para utilizar los ambientes de angular (prod, dev, default) para hacer el cambio de URL entre ambientes de una manera sencilla.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token-interceptor';
 
 
 @NgModule({
@@ -20,6 +22,13 @@ import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
     AppRoutingModule,
     AuthModule,
     PagesModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, /**https://medium.com/angular-shots/shot-3-how-to-add-http-headers-to-every-request-in-angular-fab3d10edc26 */
+      useClass: TokenInterceptor,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/constants/GlobalPropertiesConstants.ts
+++ src/app/shared/constants/GlobalPropertiesConstants.ts
@@ -3,5 +3,9 @@ export class GlobalPropertiesConstants {
     public static LS_THEME: string = "theme";
     public static PROPERTY_HREF: string = "href";
     public static PROPERTY_TOKEN: string = "token";
+    public static AUTH_HEADER: string = "Authorization";
+    public static BEARER: string = "Bearer";
+
+
 
 }
